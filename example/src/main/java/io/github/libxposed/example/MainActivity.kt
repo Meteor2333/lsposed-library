1f347985d6d38eb8f34abc17cdd56a5f89ab73f4
Meteor2333 <Meteor23333@outlook.com>
2026-05-17 13:20:05 +0800

    ref: 重写example模块

--- example/src/main/java/io/github/libxposed/example/MainActivity.kt
+++ example/src/main/java/io/github/libxposed/example/MainActivity.kt
@@ -6,35 +6,34 @@ import android.os.Bundle
 import android.widget.Toast
 import io.github.libxposed.example.databinding.ActivityMainBinding
 import io.github.libxposed.service.XposedService
-import io.github.libxposed.service.XposedService.OnScopeEventListener
+import io.github.libxposed.service.callback.ScopeEventCallback
 import java.io.FileWriter
 import kotlin.random.Random
 
 @SuppressLint("SetTextI18n")
-class MainActivity : Activity(), App.ServiceStateListener {
-    private var mService: XposedService? = null
+class MainActivity : Activity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val mCallback = object : OnScopeEventListener {
-        override fun onScopeRequestApproved(approved: List<String>) {
+    private val mCallback = object : ScopeEventCallback() {
+        override fun onApproved(packageName: String) {
             runOnUiThread {
                 Toast.makeText(
                     this@MainActivity,
-                    "onScopeRequestApproved: $approved",
+                    "onApproved: $packageName",
                     Toast.LENGTH_SHORT
                 ).show()
-                binding.scope.text = "Scope: " + mService?.scope
+                binding.scope.text = "Scopes: " + XposedService.getScopes()
             }
         }
 
-        override fun onScopeRequestFailed(message: String) {
+        override fun onFailed(message: String) {
             runOnUiThread {
                 Toast.makeText(
                     this@MainActivity,
-                    "onScopeRequestFailed: $message",
+                    "onFailed: $message",
                     Toast.LENGTH_SHORT
                 ).show()
-                binding.scope.text = "Scope: " + mService?.scope
+                binding.scope.text = "Scopes: " + XposedService.getScopes()
             }
         }
     }
@@ -42,65 +41,27 @@ class MainActivity : Activity(), App.ServiceStateListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.let {
-            setContentView(it.root)
-            it.binder.text = "Loading"
+        setContentView(binding.root)
+        binding.binder.text = "Binder acquired"
+        binding.api.text = "API " + XposedService.getApiVersion()
+        binding.framework.text = "Framework " + XposedService.getFrameworkName()
+        binding.frameworkVersion.text = "Framework version " + XposedService.getFrameworkVersion()
+        binding.frameworkVersionCode.text = "Framework version code " + XposedService.getFrameworkVersionCode()
+        binding.scope.text = "Scopes: " + XposedService.getScopes()
+        binding.requestScope.setOnClickListener {
+            XposedService.requestScope("com.android.settings", mCallback)
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        App.addServiceStateListener(this, true)
-    }
-
-    override fun onStop() {
-        App.removeServiceStateListener(this)
-        super.onStop()
-    }
-
-    override fun onServiceStateChanged(service: XposedService?) {
-        mService = service
-        runOnUiThread {
-            if (service == null) {
-                binding.binder.text = "Binder is null"
-            } else {
-                binding.binder.text = "Binder acquired"
-                binding.api.text = "API " + service.apiVersion
-                binding.framework.text = "Framework " + service.frameworkName
-                binding.frameworkVersion.text = "Framework version " + service.frameworkVersion
-                binding.frameworkVersionCode.text =
-                    "Framework version code " + service.frameworkVersionCode
-                val cap = service.frameworkProperties
-                val capStringList = mutableListOf<String>()
-                if (cap.and(XposedService.PROP_CAP_SYSTEM) != 0L) {
-                    capStringList.add("PROP_CAP_SYSTEM")
-                }
-                if (cap.and(XposedService.PROP_CAP_REMOTE) != 0L) {
-                    capStringList.add("PROP_CAP_REMOTE")
-                }
-                if (cap.and(XposedService.PROP_RT_API_PROTECTION) != 0L) {
-                    capStringList.add("PROP_RT_API_PROTECTION")
-                }
-                binding.frameworkProperties.text =
-                    "Framework properties: $capStringList"
-                binding.scope.text = "Scope: " + service.scope
-
-                binding.requestScope.setOnClickListener {
-                    service.requestScope(listOf("com.android.settings"), mCallback)
-                }
-                binding.randomPrefs.setOnClickListener {
-                    val prefs = service.getRemotePreferences("test")
-                    val old = prefs.getInt("test", -1)
-                    val new = Random.nextInt()
-                    Toast.makeText(this@MainActivity, "$old -> $new", Toast.LENGTH_SHORT).show()
-                    prefs.edit()?.putInt("test", new)?.apply()
-                }
-                binding.remoteFile.setOnClickListener {
-                    service.openRemoteFile("test.txt").use { pfd ->
-                        FileWriter(pfd.fileDescriptor).use {
-                            it.append("Hello World!")
-                        }
-                    }
+        binding.randomPrefs.setOnClickListener {
+            val prefs = XposedService.getRemotePreferences("test")
+            val old = prefs.getInt("test", -1)
+            val new = Random.nextInt()
+            Toast.makeText(this@MainActivity, "$old -> $new", Toast.LENGTH_SHORT).show()
+            prefs.edit()?.putInt("test", new)?.apply()
+        }
+        binding.remoteFile.setOnClickListener {
+            XposedService.openRemoteFile("test.txt").use { pfd ->
+                FileWriter(pfd.fileDescriptor).use {
+                    it.append("Hello World!")
                 }
             }
         }
--- example/src/main/java/io/github/libxposed/example/ModuleMain.kt
+++ example/src/main/java/io/github/libxposed/example/ModuleMain.kt
@@ -1,92 +1,48 @@
-package io.github.libxposed.example;
+package io.github.libxposed.example
 
-import android.os.Build;
-import android.util.Log;
+import android.util.Log
+import io.github.libxposed.api.XposedModule
+import io.github.libxposed.api.XposedModuleInterface
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
+class ModuleMain : XposedModule() {
+    companion object {
+        const val TAG = "XposedExample"
 
-import java.util.Locale;
-
-import io.github.libxposed.api.XposedModule;
-
-public class ModuleMain extends XposedModule {
-    static final String TAG = "ModuleMain";
-
-    private boolean hasProp(long prop) {
-        return (getFrameworkProperties() & prop) != 0;
-    }
-
-    @Override
-    public void onModuleLoaded(@NonNull ModuleLoadedParam param) {
-        log(Log.INFO, TAG, "onModuleLoaded: " + param.getProcessName());
-        log(Log.INFO, TAG, String.format(Locale.getDefault(), "framework: %s (%s) API %d", getFrameworkName(), getFrameworkVersionCode(), getApiVersion()));
-        log(Log.INFO, TAG, "system supported: " + hasProp(PROP_CAP_SYSTEM));
-        log(Log.INFO, TAG, "remote supported: " + hasProp(PROP_CAP_REMOTE));
-        log(Log.INFO, TAG, "api protection: " + hasProp(PROP_RT_API_PROTECTION));
-    }
-
-    @Override
-    @RequiresApi(Build.VERSION_CODES.Q)
-    public void onPackageLoaded(@NonNull PackageLoadedParam param) {
-        log(Log.INFO, TAG, "onPackageLoaded: " + param.getPackageName());
-        log(Log.INFO, TAG, "default classloader is " + param.getDefaultClassLoader());
-    }
-
-    @Override
-    public void onPackageReady(@NonNull PackageReadyParam param) {
-        try {
-            var exampleClass = Class.forName("io.github.libxposed.example.Example", true, param.getClassLoader());
-            var exampleMethod = exampleClass.getDeclaredMethod("method");
-            var exampleConstructor = exampleClass.getDeclaredConstructor();
-
-            hook(exampleMethod).intercept(chain -> {
-                log(Log.INFO, TAG, "call the following chains with the same args");
-                var result = (String) chain.proceed();
-
-                log(Log.INFO, TAG, "call the following chains with different args");
-                String old0 = (String) chain.getArg(0);
-                Object new1 = new Object();
-                var newArgs = new Object[]{old0, new1};
-                result += (String) chain.proceed(newArgs);
-
-                log(Log.INFO, TAG, "call the following chains with different this object");
-                var newThis = new Object();
-                result += (String) chain.proceedWith(newThis);
-                result += (String) chain.proceedWith(newThis, newArgs);
-
-                log(Log.INFO, TAG, "call the raw method");
-                result += (String) getInvoker(exampleMethod).setType(Invoker.Type.ORIGIN).invoke(chain.getThisObject());
-
-                return result;
-            });
-
-            hook(exampleMethod).intercept(chain -> {
-                chain.proceed();
-                // for void methods, it's needed to return null because Java doesn't support unit type
-                return null;
-            });
-
-            hook(exampleConstructor)
-                    .setPriority(PRIORITY_HIGHEST)
-                    .setExceptionMode(ExceptionMode.PASSTHROUGH)
-                    .intercept(chain -> {
-                        log(Log.INFO, TAG, "thrown exception will be propagated to upper interceptors or the caller");
-                        throw new RuntimeException("constructor hook exception");
-                    });
-
-            // call the original method
-            getInvoker(exampleMethod).setType(Invoker.Type.ORIGIN).invoke(new Object());
-            // call the special method starting from the middle of the hook chain
-            getInvoker(exampleMethod).setType(new Invoker.Type.Chain(-50)).invokeSpecial(new Object());
-            // create a new instance using the original constructor
-            getInvoker(exampleConstructor).setType(Invoker.Type.ORIGIN).newInstance();
-            // create a new special instance with full hook chain
-            getInvoker(exampleConstructor).setType(Invoker.Type.Chain.FULL).newInstanceSpecial(exampleClass);
-            // identical to the above line, default to call with full hook chain
-            getInvoker(exampleConstructor).newInstanceSpecial(exampleClass);
-        } catch (Throwable t) {
-            log(Log.ERROR, TAG, "Error in onPackageLoaded", t);
+        private fun log(msg: String) {
+            Log.i(TAG, msg)
         }
+    }
+
+    override fun onModuleLoaded(param: XposedModuleInterface.ModuleLoadedParam) {
+        log("onModuleLoaded: " + param.processName)
+        log("framework: $frameworkName($frameworkVersionCode)")
+    }
+
+    override fun onPackageLoaded(param: XposedModuleInterface.PackageLoadedParam) {
+        log("onPackageLoaded: " + param.packageName)
+        log("default classloader is " + param.defaultClassLoader)
+        log("----------")
+
+        if (!param.isFirstPackage) return
+
+        val prefs = getRemotePreferences("test")
+        log("remote prefs: " + prefs.getInt("test", -1))
+        prefs.registerOnSharedPreferenceChangeListener { _, key ->
+            val value = prefs.getInt(key, 0)
+            log("onSharedPreferenceChanged: $key->$value")
+        }
+
+//        try {
+//            val text = openRemoteFile("test.txt").use {
+//                FileReader(it.fileDescriptor).readText()
+//            }
+//            log("remote file content: $text")
+//        } catch (_: FileNotFoundException) {
+//            log("remote file not found")
+//        }
+    }
+
+    override fun onSystemServerLoaded(param: XposedModuleInterface.SystemServerLoadedParam) {
+        log("onSystemServerStarting, system classloader: " + param.classLoader)
     }
 }
