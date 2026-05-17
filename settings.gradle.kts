6bf74bbb3efdb981a836dca9cc8818049a02cbe6
Meteor2333 <Meteor23333@outlook.com>
2026-05-17 11:40:18 +0800

    ref: 新建gradle父模块

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lsposed-lib"
+include(":lsposed-api")
+project(":lsposed-api").projectDir = file("api")
+include(":lsposed-service")
+project(":lsposed-service").projectDir = file("service")
+include(":lsposed-example")
+project(":lsposed-example").projectDir = file("example")
